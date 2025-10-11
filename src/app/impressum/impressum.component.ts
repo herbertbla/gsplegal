76b11c1266b868a6023ab418ac186a52bf7d20b1
jupit <jupiter@gmx.at>
2025-10-11 17:38:07 +0200

    Rework 2025 - impressum

--- src/app/impressum/impressum.component.ts
+++ src/app/impressum/impressum.component.ts
@@ -1,16 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
-    selector: 'app-impressum',
-    templateUrl: './impressum.component.html',
-    styleUrls: ['./impressum.component.less'],
-    standalone: false
+  selector: 'app-impressum',
+  templateUrl: './impressum.component.html',
+  styleUrls: ['./impressum.component.less'],
+  standalone: false
 })
 export class ImpressumComponent implements OnInit {
+  currentLang: string;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private translate: TranslateService) {
+    this.currentLang = this.translate.currentLang || 'de';
   }
 
+  ngOnInit() {
+    this.translate.onLangChange.subscribe(event => {
+      this.currentLang = event.lang;
+    });
+  }
 }
