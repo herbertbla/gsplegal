3ca4b702f8aad007a6a6fe39bdc21af78627fdf6
jupit <jupiter@gmx.at>
2025-10-11 17:48:08 +0200

    Rework 2025 - Haftung

--- src/app/haftungsausschluss/haftungsausschluss.component.ts
+++ src/app/haftungsausschluss/haftungsausschluss.component.ts
@@ -1,16 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
-    selector: 'app-haftungsausschluss',
-    templateUrl: './haftungsausschluss.component.html',
-    styleUrls: ['./haftungsausschluss.component.less'],
-    standalone: false
+  selector: 'app-haftungsausschluss',
+  templateUrl: './haftungsausschluss.component.html',
+  styleUrls: ['./haftungsausschluss.component.less'],
+  standalone: false
 })
 export class HaftungsausschlussComponent implements OnInit {
+  currentLang: string;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private translate: TranslateService) {
+    this.currentLang = this.translate.currentLang || 'de';
   }
 
+  ngOnInit() {
+    this.translate.onLangChange.subscribe(event => {
+      this.currentLang = event.lang;
+    });
+  }
 }
