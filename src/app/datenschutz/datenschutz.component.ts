a13d6834f35a2f89a52ff5f3c3b49804294fdfa3
jupit <jupiter@gmx.at>
2025-10-11 17:54:39 +0200

    Rework 2025 - Datenschutz

--- src/app/datenschutz/datenschutz.component.ts
+++ src/app/datenschutz/datenschutz.component.ts
@@ -1,16 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
-    selector: 'app-datenschutz',
-    templateUrl: './datenschutz.component.html',
-    styleUrls: ['./datenschutz.component.less'],
-    standalone: false
+  selector: 'app-datenschutz',
+  templateUrl: './datenschutz.component.html',
+  styleUrls: ['./datenschutz.component.less'],
+  standalone: false
 })
 export class DatenschutzComponent implements OnInit {
+  currentLang: string;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private translate: TranslateService) {
+    this.currentLang = this.translate.currentLang || 'de';
   }
 
+  ngOnInit() {
+    this.translate.onLangChange.subscribe(event => {
+      this.currentLang = event.lang;
+    });
+  }
 }
