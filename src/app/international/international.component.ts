e398d7bbb769fc7b6666a0cbe9af42bf95f49c70
jupit <jupiter@gmx.at>
2025-10-11 17:27:11 +0200

    Rework 2025 - international

--- src/app/international/international.component.ts
+++ src/app/international/international.component.ts
@@ -1,16 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { Location } from '@angular/common';
+import { TranslateService } from '@ngx-translate/core';
+
 
 @Component({
-    selector: 'app-international',
-    templateUrl: './international.component.html',
-    styleUrls: ['./international.component.less'],
-    standalone: false
+  selector: 'app-international',
+  templateUrl: './international.component.html',
+  styleUrls: ['./international.component.less'],
+  standalone: false
 })
 export class InternationalComponent implements OnInit {
+  currentLang: string;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private translate: TranslateService) {
+    this.currentLang = this.translate.currentLang || 'de';
   }
 
+  ngOnInit() {
+    this.translate.onLangChange.subscribe(event => {
+      this.currentLang = event.lang;
+    });
+  }
 }
